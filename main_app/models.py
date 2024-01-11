23505b848a3cd7560c9cea91394dfb941b6346c6
NathanielSimmons <nathaniel.cj.simmons@gmail.com>
2024-01-11 18:54:41 -0500

    Finished Part#4

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
 
 class Finch (models. Model):
     name= models.CharField(max_length=100)
@@ -14,3 +18,24 @@ class Finch (models. Model):
 
     def get_absolute_url(self):
         return reverse('detail',kwargs={'finch_id':self.id})
+    
+class Feeding(models.Model):
+    date = models.DateField('feeding date')
+    meal = models.CharField(
+        max_length=1,
+    # add the 'choices' field option
+        choices=MEALS,
+    # set the default value for meal to be 'B'
+        default=MEALS[0][0]
+        )
+  
+  # Create a cat_id FK
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+    def __str__(self):
+    # Nice method for obtaining the friendly value of a Field.choice
+        return f"{self.get_meal_display()} on {self.date}"
+  
+  # change the default sort
+    class Meta:
+        ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from.models import Finch
+from .forms import FeedingForm
 
 # Create your views here.
 def home(request):
@@ -15,7 +16,20 @@ def all_finches(request):
 
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
-  return render(request, 'finches/detail.html', { 'finch': finch })
+  feeding_form = FeedingForm()
+  return render(request, 'finches/detail.html', { 'finch': finch, 'feeding_form': feeding_form })
+
+def add_feeding(request, finch_id):
+  # create a ModelForm instance using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the cat_id assigned
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
 
 class FinchCreate(CreateView):
   model = Finch
@@ -23,7 +37,7 @@ class FinchCreate(CreateView):
 
 class FinchUpdate(UpdateView):
   model = Finch
-  # Let's disallow the renaming of a cat by excluding the name field!
+  # Let's disallow the renaming of a finch by excluding the name field!
   fields = ['breed', 'gender', 'color']
 
 class FinchDelete(DeleteView):
