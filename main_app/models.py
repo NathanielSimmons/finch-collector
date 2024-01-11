2f5f6d562986e5d82cf50773208cdd78e6a39036
NathanielSimmons <nathaniel.cj.simmons@gmail.com>
2024-01-11 12:21:59 -0500

    Finished Part#3

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -8,5 +9,8 @@ class Finch (models. Model):
     gender = models.TextField(max_length=250)
     color = models.CharField(max_length=100)
 
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail',kwargs={'finch_id':self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from.models import Finch
 
 # Create your views here.
@@ -15,3 +16,16 @@ def all_finches(request):
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
   return render(request, 'finches/detail.html', { 'finch': finch })
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = ['breed', 'gender', 'color']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches'
